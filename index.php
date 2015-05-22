1695a44b0f1d38a0d926710d9fc236db2b1192a4
Huy Pham <huyphamc@gmail.com>
2015-05-22 22:54:34 +0700

    Version 1.0

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Check IPs</title>
+		<title>Upload File Ajax jQuery</title>
 
 		<!-- Bootstrap CSS -->
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
@@ -15,23 +15,76 @@
 			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		<style type="text/css" media="screen">
+			div#wrap {
+				width: 600px;
+				margin: 0 auto;
+			}
+			.btn-file {
+			    position: relative;
+			    overflow: hidden;
+			}
+			.btn-file input[type=file] {
+			    position: absolute;
+			    top: 0;
+			    right: 0;
+			    min-width: 100%;
+			    min-height: 100%;
+			    font-size: 100px;
+			    text-align: right;
+			    filter: alpha(opacity=0);
+			    opacity: 0;
+			    outline: none;
+			    background: white;
+			    cursor: inherit;
+			    display: block;
+			}
+			#image-list{
+				list-style: none;
+			}
+			#image-list img{
+				width: 100%;
+				height: auto;
+			}
+		</style>
 	</head>
 	<body>
-		<h1 class="text-center">Check Ips</h1>
-		
-		<form id="upload_csv" class="form-inline" action="upload.php" method="post" enctype="multipart/form-data">
-		    <input type="text" name="text_val" value="text_value_fjdslakfjdkl">
-		    <textarea name="text_area">text_area_fjdsalkfjdkfl</textarea>
-		    <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" multiple>
-		    <input id="btn" class="btn" type="submit" value="Upload Image" name="submit">
-		</form>
-		
+		<div id="wrap">
+			<h1 class="text-center">Upload File Ajax jQuery</h1>
+			
+			<form id="upload_file" class="form-horizontal" action="upload.php" method="post" enctype="multipart/form-data">
+			    <div class="form-group">
+			    	<label for="exampleInputEmail1">Text</label>
+			    	<input class="form-control" type="text" name="text_val" value="text_content">
+			    </div>
+			    <div class="form-group">
+			    	<label for="exampleInputEmail1">Text Area</label>
+			    	<textarea class="form-control" rows="3" name="text_area">text_area_content</textarea>
+			    </div>
+			    <div class="form-group">
+		            <div class="input-group">
+		                <span class="input-group-btn">
+		                    <span class="btn btn-primary btn-file">
+		                        Browse&hellip; <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" multiple>
+		                    </span>
+		                </span>
+		            </div>
+		            <ul id="image-list">
+
+					</ul>
+				</div>
+				<div class="form-group">
+			    	<input id="btn" class="btn" type="submit" value="Upload File" name="submit">
+			    </div>
+			</form>
+		</div>
 		<!-- jQuery -->
 		<script src="//code.jquery.com/jquery.js"></script>
 		<!-- Bootstrap JavaScript -->
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 		<!-- Scripts -->
 		<script src="scripts.js"></script>
+
 	</body>
 </html>
 <?php
--- scripts.js
+++ scripts.js
@@ -1,6 +1,35 @@
 jQuery(function(){
+	//Display if that's image
+	function showUploadedItem (source, name) {
+  		console.log(source);
+  		console.log(name);
+  		if(source != "")
+  			jQuery('#image-list').append('<li><img src="'+source+'" /></li>');
+  		else
+  			jQuery('#image-list').append('<li>'+name+'</li>');
+	}   
+	jQuery("#fileToUpload").on('change',function(){
+		jQuery('#image-list').html("");
+		var i = 0, len = jQuery("#fileToUpload")[0].files.length;		
+		for( ; i < len ; i++){
+			file = jQuery("#fileToUpload")[0].files[i];
+			if (!!file.type.match(/image.*/)) {
+				if ( window.FileReader ) {
+					reader = new FileReader();
+					reader.onloadend = function (e) { 
+						showUploadedItem(e.target.result, file.name);
+					};
+					reader.readAsDataURL(file);
+				}
+			}else{
+				showUploadedItem("", file.name);
+			}	
+				
+		}
 
-	jQuery('#upload_csv').submit(function(evt){
+	})
+	//Submit Action
+	jQuery('#upload_file').submit(function(evt){
 		evt.preventDefault();
 		
 		var i = 0, len = jQuery("#fileToUpload")[0].files.length;
@@ -8,7 +37,7 @@ jQuery(function(){
 
 		for( ; i < len ; i++){
 			file = jQuery("#fileToUpload")[0].files[i];
-			formdata.append("fileToUpload[]", file);	
+			formdata.append("fileToUpload[]", file);
 		}
 		var val = jQuery(this).serializeArray();
 		var i = 0, len = val.length;
